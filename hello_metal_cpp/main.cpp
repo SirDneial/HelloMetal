93f265b8bd2932cc9ed9b7f3390cf5caf382bab8
Dneial <111139207+DRiveraa@users.noreply.github.com>
2024-02-15 17:04:30 -0500

    Initial Commit

---
+++ hello_metal_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello_metal_cpp
+//
+//  Created by Daniel Rivera on 2/15/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
