18c1bca99e1bf12c81d077e947fcda50b0bfbdc6
Dneial <111139207+DRiveraa@users.noreply.github.com>
2024-02-15 18:52:19 -0500

    init project setup

--- hello_metal_cpp/main.cpp
+++ hello_metal_cpp/main.cpp
@@ -5,10 +5,25 @@
 //  Created by Daniel Rivera on 2/15/24.
 //
 
-#include <iostream>
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#define MTK_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+
+#include "config.h"
+#include "control/app_delegate.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    NS::AutoreleasePool* autoreleasePool = NS::AutoreleasePool::alloc()->init();
+    
+    AppDelegate controller;
+    
+    NS::Application* app = NS::Application::sharedApplication();
+    app->setDelegate(&controller);
+    app->run();
+    
+    autoreleasePool->release();
+    
     return 0;
 }
